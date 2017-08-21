06a1ccf398654ceee507199ff183b4a184d3d468
Jannis R <mail@jannisr.de>
2017-08-21 02:36:28 +0200

    patch: copy, dryRun, bugfix :bug:

--- patch.js
+++ patch.js
@@ -3,16 +3,25 @@
 const eachSeries = require('async/eachSeries')
 
 const createTypeIndex = require('./lib/type-index')
+const createGet = require('./get')
+const createPut = require('./put')
 const createDel = require('./del')
 const {arrayIndex, ARRAY, OBJECT, OTHER} = createTypeIndex
 
 // applies a JSON patch to a level-tree
 // see http://jsonpatch.com
 const createPatch = (db) => {
+	const get = createGet(db)
+	const put = createPut(db)
 	const del = createDel(db)
-	let ops = []
 
-	const applyAll = (ns, patches, cb) => {
+	const applyAll = (ns, patches, dryRun, cb) => {
+		if ('function' === typeof dryRun) {
+			cb = dryRun
+			dryRun = false
+		}
+		let ops = []
+
 		createTypeIndex(db, ns, (err, typeAt) => {
 			if (err) return cb(err)
 
@@ -20,6 +29,7 @@ const createPatch = (db) => {
 				const path = patch.path.split('/').slice(1)
 				path.unshift(ns)
 				const last = path[path.length - 1]
+				const key = path.join('.')
 
 				if (patch.op === 'add') {
 					// check if there is an existing conflicting leaf
@@ -27,19 +37,40 @@ const createPatch = (db) => {
 					const newType = arrayIndex.test(last) ? ARRAY : OBJECT
 					if (existingType !== newType) {
 						// todo: more helpful message
+						// todo: DRY with copy
 						return cb(new Error('conflict at ' + patch.path))
 					}
 
-					ops.push({type: 'put', key: path.join('.'), value: patch.value})
+					ops.push({type: 'put', key, value: patch.value})
 					cb()
 				} else if (patch.op === 'remove') {
 					// todo: optimize using the typeAt index
-					const key = path.join('.')
 					del(key, true, (err, delOps) => {
 						if (err) return cb(err)
 
 						ops = ops.concat(delOps)
 						cb()
+					})
+				} else if (patch.op === 'copy') {
+					// check if there is an existing conflicting leaf
+					const existingType = typeAt[path.slice(0, -1).join('.')] // without leaf
+					const newType = arrayIndex.test(last) ? ARRAY : OBJECT
+					if (existingType !== newType) {
+						// todo: more helpful message
+						// todo: DRY with add
+						return cb(new Error('conflict at ' + patch.path))
+					}
+
+					const from = patch.from.split('/').slice(1)
+					from.unshift(ns)
+
+					get(from.join('.'), (err, tree) => {
+						if (err) return cb(err)
+						put(key, tree, true, (err, putOps) => {
+							if (err) return cb(err)
+							ops = ops.concat(putOps)
+							cb()
+						})
 					})
 				} else {
 					// todo: replace, remove, copy, move, test
@@ -50,7 +81,8 @@ const createPatch = (db) => {
 			eachSeries(patches, apply, (err) => {
 				if (err) return cb(err)
 
-				db.batch(ops, cb) // todo: dryRun
+				if (dryRun) cb(null, ops)
+				else db.batch(ops, cb)
 			})
 		})
 	}
--- test/patch.js
+++ test/patch.js
@@ -13,7 +13,7 @@ test('patch', (t) => {
 	const patch = createPatch(db)
 
 	t.equal(typeof patch, 'function')
-	t.equal(patch.length, 3)
+	t.equal(patch.length, 4)
 })
 
 test('patch: add works', (t) => {
@@ -87,6 +87,72 @@ test('patch: remove works', (t) => {
 				const i = dataI++
 				t.deepEqual(data, expected[i], i + ' looks good')
 			})
+		})
+	})
+})
+
+test('patch: copy works', (t) => {
+	const expected = sortBy([
+		{key: 't.a1', value: 'A1'},
+		{key: 't.a2.0', value: 'A2-0'},
+		{key: 't.a2.1.b1', value: 'A2-1-B1'},
+		{key: 't.a2.1.b2', value: 'A2-1-B2'},
+		{key: 't.a3', value: 'A3'},
+		{key: 't.a4.b1', value: 'A2-1-B1'}, // note the value
+		{key: 't.a4.b2', value: 'A2-1-B2'}, // note the value
+	], 'key')
+	t.plan(expected.length)
+
+	const db = levelup(memdown)
+	const patch = createPatch(db)
+
+	db.batch()
+		.put('t.a1', 'A1')
+		.put('t.a2.0', 'A2-0')
+		.put('t.a2.1.b1', 'A2-1-B1')
+		.put('t.a2.1.b2', 'A2-1-B2')
+		.put('t.a3', 'A3')
+	.write((err) => {
+		if (err) return t.ifError(err)
+
+		patch('t', [
+			{op: 'copy', path: '/a4', from: '/a2/1'}
+		], (err) => {
+			if (err) return t.ifError(err)
+
+			let dataI = 0
+			db.createReadStream()
+			.once('error', t.ifError)
+			.on('data', (data) => {
+				const i = dataI++
+				t.deepEqual(data, expected[i], i + ' looks good')
+			})
+		})
+	})
+})
+
+test('patch: dryRun works', (t) => {
+	t.plan(1)
+
+	const db = levelup(memdown)
+	const patch = createPatch(db)
+
+	db.batch()
+		.put('t.a1', 'A1')
+		.put('t.a2.0', 'A2-0')
+		.put('t.a2.1.b1', 'A2-1-B1')
+		.put('t.a3', 'A3')
+	.write((err) => {
+		if (err) return t.ifError(err)
+
+		patch('t', [
+			{op: 'add', path: '/a4', value: 'new A4'}
+		], true, (err, ops) => {
+			if (err) return t.ifError(err)
+
+			t.deepEqual(ops, [
+				{type: 'put', key: 't.a4', value: 'new A4'}
+			])
 		})
 	})
 })
